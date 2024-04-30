c465beae43e9b3c0be2e01b52b147091ad03eecc
Ligar Arnata <ligararnata@gmail.com>
2024-05-01 00:03:16 +0700

    sixth change

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
         <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 blur-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
         <div className="z-10 text-center p-6 bg-white bg-opacity-0 rounded-lg shadow">
           <h5 className="text-xl font-bold text-white my-2">
-            Let's Connect
+            Let&apos;s Connect
           </h5>
           <p className="text-[#ADB7BE] mb-4 max-w-md">
             I am currently looking for new opportunities, my inbox is always
