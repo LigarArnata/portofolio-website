019fdce1b66d87763145aa1487f7c65affc47044
Ligar Arnata <ligararnata@gmail.com>
2024-04-30 23:51:37 +0700

    fourth change

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-center">
         <span><Image src="/images/logo.png" width={80}
-          height={80}></Image></span>
+          height={80} alt='foto'></Image></span>
       </div>
     </footer>
   )
